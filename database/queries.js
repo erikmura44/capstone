f6b847f880ec9614cd2433cc19a2535c0ee2d8d0
erikmura44 <erikmura44@gmail.com>
2016-11-29 15:49:17 -0700

    updated queries

--- database/queries.js
+++ database/queries.js
@@ -4,6 +4,35 @@ function getPlayer (player) {
   return knex('players')
 }
 
+function getBlackTees (course_id) {
+   return knex('black').first().where("course_id", course_id).then(function(data){
+      data.color = 'black'
+      return data
+   })
+}
+function getBlueTees (course_id) {
+   return knex('blue').first().where("course_id", course_id).then(function(data){
+      data.color = 'blue'
+      return data
+   })
+}
+function getWhiteTees (course_id) {
+   return knex('white').first().where("course_id", course_id).then(function(data){
+      data.color = 'white'
+      return data
+  })
+}
+function getRedTees (course_id) {
+   return knex('red').first().where("course_id", course_id).then(function(data){
+      data.color = 'red'
+      return data
+   })
+}
+
 module.exports = {
-  getPlayer: getPlayer
+  getPlayer: getPlayer,
+  getBlackTees: getBlackTees,
+  getBlueTees: getBlueTees,
+  getWhiteTees: getWhiteTees,
+  getRedTees: getRedTees
 }
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,15 @@ var router = express.Router();
 var queries = require('../database/queries.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-   queries.getPlayer()
+router.get('/:course_id', function(req, res, next) {
+   Promise.all(
+      [
+         queries.getBlackTees(req.params.course_id),
+         queries.getBlueTees(req.params.course_id),
+         queries.getWhiteTees(req.params.course_id),
+         queries.getRedTees(req.params.course_id)
+      ]
+   )
    .then(function(data){
       res.json(data)
    })
@@ -12,6 +19,14 @@ router.get('/', function(req, res, next) {
       res.status(500).json(error.message)
    })
 });
+
+router.get('/', function(req, res, next){
+   Promise.all(
+      [
+         queries.getPlayer()
+      ]
+   )
+})
 
 
 module.exports = router;
