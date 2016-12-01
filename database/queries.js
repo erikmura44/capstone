e5ef43e1070b023f87115f958e7a7a492a4ef3fb
erikmura44 <erikmura44@gmail.com>
2016-12-01 14:45:13 -0700

    new queries

--- database/queries.js
+++ database/queries.js
@@ -38,6 +38,14 @@ function sendScoresToDB (game){
    return knex('games').insert(game)
 }
 
+function gameData (game){
+   return knex('games').innerJoin('courses', 'courses.id', 'games.course_id')
+}
+
+function handicap (game){
+   return knex.raw(`SELECT AVG((score1 + score2 + score3 + score4 + score5 + score6 + score7 + score8 + score9 + score10 + score11 + score12 + score13 + score14 + score15 + score16 + score17 + score18) - 72) AS handicap FROM games`)
+}
+
 module.exports = {
   getPlayer: getPlayer,
   getBlackTees: getBlackTees,
@@ -45,5 +53,7 @@ module.exports = {
   getWhiteTees: getWhiteTees,
   getRedTees: getRedTees,
   getPar: getPar,
-  sendScoresToDB: sendScoresToDB
+  sendScoresToDB: sendScoresToDB,
+  gameData: gameData,
+  handicap: handicap
 }
